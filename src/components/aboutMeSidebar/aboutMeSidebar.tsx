5b79dc9043528156041fd5d4d87d3aa535ac9c96
callmeJOUYA <mohammad.pr285@gmail.com>
2025-01-20 16:51:29 +0330

    side bar finished and animated

--- src/components/aboutMeSidebar/aboutMeSidebar.tsx
+++ src/components/aboutMeSidebar/aboutMeSidebar.tsx
@@ -1,15 +1,21 @@
+'use client'
+import { api } from "@/api/api";
+import useAxios from "@/hooks/useAxios";
+import { Bolt, Hiking, PersonSearch, School, Translate } from '@mui/icons-material';
+import SideBarItem from './sideBarItem';
 
 
 const AboutMeSideBar = () => {
+  const { data, loading } = useAxios(api, "/hard");
+
+  const hardSkills =data.data
   return (
-    <div className="h-auto border-r col-span-2 sticky bottom-0">
-      <div>
-        <a href="#about">About me</a>
-      </div>
-      <div>
-        <a href="#hard">Hard skills</a>
-      </div>
-      <div></div>
+    <div className="h-[89vh] top-[11vh] border-r col-span-2 sticky bottom-0">
+      <SideBarItem href="#about" name="About me" icon={<PersonSearch />} />
+      <SideBarItem href="#hard" name="Hard skills" icon={<Bolt />} collapsible={true} data={hardSkills} />
+      <SideBarItem href="#education" name="Educations" icon={<School />} />
+      <SideBarItem href="#soft" name="Soft skills" icon={<Hiking />} />
+      <SideBarItem href="#languages" name="Languages" icon={<Translate />} />
     </div>
   );
 }
