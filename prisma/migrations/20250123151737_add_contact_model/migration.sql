73048aa24dc15f5bdead1fd8ed55a41bc34acc7b
callmeJOUYA <mohammad.pr285@gmail.com>
2025-01-23 19:06:08 +0330

    a new prisma model for contact

---
+++ prisma/migrations/20250123151737_add_contact_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "whats_app" TEXT NOT NULL,
+    "gmail" TEXT NOT NULL,
+    "instagram" TEXT NOT NULL,
+    "linkedin" TEXT NOT NULL,
+    "github" TEXT NOT NULL,
+    "telegram" TEXT NOT NULL
+);
