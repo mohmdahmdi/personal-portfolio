145161ad349179edb756658d061fc89ad3662f7b
callmeJOUYA <mohammad.pr285@gmail.com>
2025-01-27 14:34:28 +0330

    importing contact me components to contact me page

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -1,8 +1,12 @@
+import Contatcs from "../../../components/contacts/contacts";
 const Contact = () => {
   return (
     <>
       <div className="">
-        <div className="text-center text-xl font font-extrabold">Contact with me!</div>
+        <div className="text-center text-xl font font-extrabold mt-[17vh] mb-10">
+          Contact with me!
+        </div>
+        <Contatcs />
       </div>
     </>
   );
