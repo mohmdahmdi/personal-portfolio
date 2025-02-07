462504f8c77bc04790f83d1bd6c00d2bf26e828d
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-07 20:09:03 +0330

    working on responsive of about me side bar

--- src/app/(main)/about/layout.tsx
+++ src/app/(main)/about/layout.tsx
@@ -1,12 +1,25 @@
-import AboutMeSideBar from '../../../components/aboutMeSidebar/aboutMeSidebar';
+"use client";
+import { useState } from "react";
+import AboutMeSideBar from "../../../components/aboutMeSidebar/aboutMeSidebar";
+import { NavigateBefore, NavigateNext } from "@mui/icons-material";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [side, setSide] = useState<boolean>(false)
   return (
-    <div className='row-span-8 grid grid-cols-12'>
-      <AboutMeSideBar />
+    <div className="row-span-8 grid grid-cols-12">
+      <AboutMeSideBar side={side} setSide={setSide} />
+      <div
+        className={`absolute left-[${side ? '25' : '25'}%] cursor-pointer z-20`}
+        onClick={() => {
+          setSide(!side)
+        }}
+      >
+        {side ? <NavigateBefore /> : <NavigateNext />}
+      </div>
       {children}
     </div>
   );
--- src/components/aboutMeSidebar/aboutMeSidebar.tsx
+++ src/components/aboutMeSidebar/aboutMeSidebar.tsx
@@ -1,24 +1,44 @@
-'use client'
+"use client";
 import { api } from "@/api/api";
 import useAxios from "@/hooks/useAxios";
-import { Bolt, Hiking, PersonSearch, School, Translate } from '@mui/icons-material';
-import SideBarItem from './sideBarItem';
-import { useState } from "react";
+import {
+  Bolt,
+  Hiking,
+  PersonSearch,
+  School,
+  Translate,
+} from "@mui/icons-material";
+import SideBarItem from "./sideBarItem";
+import { AnimatePresence, motion } from "framer-motion";
 
-
-const AboutMeSideBar = () => {
+const AboutMeSideBar: React.FC<{ side: boolean; setSide: any }> = (props) => {
   const { data, loading } = useAxios(api, "/hard");
-  const [ showSide, setShowSide] = useState<boolean>(true)
-  const hardSkills =data.data
+  const hardSkills = data.data;
   return (
-    <div className="h-[89vh] top-[11vh] border-r col-span-2 sticky bottom-0">
-      <SideBarItem href="#about" name="About me" icon={<PersonSearch />} />
-      <SideBarItem href="#hard" name="Hard skills" icon={<Bolt />} collapsible={true} data={hardSkills} />
-      <SideBarItem href="#education" name="Educations" icon={<School />} />
-      <SideBarItem href="#soft" name="Soft skills" icon={<Hiking />} />
-      <SideBarItem href="#languages" name="Languages" icon={<Translate />} />
-    </div>
+    <AnimatePresence>
+      {props.side && (
+        <div
+          className="h-[89vh] top-[11vh] border-r col-span-2 sticky bottom-0 overflow-hidden"
+        >
+          <SideBarItem href="#about" name="About me" icon={<PersonSearch />} />
+          <SideBarItem
+            href="#hard"
+            name="Hard skills"
+            icon={<Bolt />}
+            collapsible={true}
+            data={hardSkills}
+          />
+          <SideBarItem href="#education" name="Educations" icon={<School />} />
+          <SideBarItem href="#soft" name="Soft skills" icon={<Hiking />} />
+          <SideBarItem
+            href="#languages"
+            name="Languages"
+            icon={<Translate />}
+          />
+        </div>
+      )}
+    </AnimatePresence>
   );
-}
- 
+};
+
 export default AboutMeSideBar;
